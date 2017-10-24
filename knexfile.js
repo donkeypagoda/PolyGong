49945d93f980083248aacd2c14403c36ab1e5c31
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-24 13:51:18 -0700

    doing stuff

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/polygong_dev'
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/polygong_dev'
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    seeds: {
+      directory: './prod_seed/'
+    }
+  }
+};
