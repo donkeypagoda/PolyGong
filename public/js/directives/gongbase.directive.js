3849c721b8236999f64a1d272fff0c63c8ca71db
donkeypagoda <christopherallenstewart@gmail.com>
2017-11-02 11:33:31 -0700

    fixed it real bad

--- public/js/directives/gongbase.directive.js
+++ public/js/directives/gongbase.directive.js
@@ -12,32 +12,38 @@
     controller.inject = ['helperService', 'gongBuilderService', 'toneService', 'urlService', '$state'];
     function controller(helperService, gongBuilderService, toneService, urlService, $state) {
       const vm = this;
-      vm.helper = helperService;
-      vm.tone = toneService;
-      vm.builder = gongBuilderService;
-      vm.url = urlService;
+      vm.$onInit = () => {
 
-      vm.dronePitch = 60;
-      vm.drone = vm.tone.droneBuilder(vm.dronePitch)
-      vm.drone.volume.value = -36;
-      vm.verb = vm.tone.makeVerb();
-      vm.delay = vm.tone.makeDelay();
-      vm.verb.wet.value = 0.0;
-      vm.delay.wet.value = 0.0;
+        vm.helper = helperService;
+        vm.tone = toneService;
+        vm.builder = gongBuilderService;
+        vm.url = urlService;
 
-      Tone.Master.chain(vm.delay, vm.verb);
-      // restore from url
-      if ($state.params.url){
-        // console.log($state.params.url);
-        vm.url.getState($state.params.url)
-        .then(()=>{
-          console.log(vm.url.gongData)
-          //should replace this next section with a .map function or some shit
-          vm.builder.gongStack = []
-          for (let i = 0; i < vm.url.gongData.length; i++){
-            vm.builder.gongStack.push(vm.url.gongData[i].name)
-          }
-        })
+        vm.dronePitch = 60;
+        vm.drone = vm.tone.droneBuilder(vm.dronePitch)
+        vm.drone.volume.value = -36;
+        vm.verb = vm.tone.makeVerb();
+        vm.delay = vm.tone.makeDelay();
+        vm.verb.wet.value = 0.0;
+        vm.delay.wet.value = 0.0;
+        console.log(vm.drone.volume.value);
+        console.log(vm.delay.wet.value);
+        console.log(vm.verb.wet.value);
+
+        Tone.Master.chain(vm.delay, vm.verb);
+        // restore from url
+        if ($state.params.url){
+          // console.log($state.params.url);
+          vm.url.getState($state.params.url)
+          .then(()=>{
+            console.log(vm.url.gongData)
+            //should replace this next section with a .map function or some shit
+            vm.builder.gongStack = []
+            for (let i = 0; i < vm.url.gongData.length; i++){
+              vm.builder.gongStack.push(vm.url.gongData[i].name)
+            }
+          })
+        }
       }
     } // end of controller
     function link(scope, element, iAttrs, controller, transcludeFn){
@@ -55,18 +61,30 @@
       controller.contextTop.clearRect(0, 0, controller.canvasTop.width, controller.canvasTop.height);
       controller.contextTop.translate(350, 350);
       controller.helper.gongTopLine(controller.contextTop, "blue", 5);
+      let droneInvoked = false;
+      let delayInvoked = false;
+      let verbInvoked = false;
 
       controller.droneVolume = (val) => {
-        controller.drone.volume.value = parseFloat(val);
-        // console.log(controller.drone.volume.value);
+        if (droneInvoked){
+          controller.drone.volume.value = parseFloat(val);
+          console.log(controller.drone.volume.value);
+        }
+        else droneInvoked = true;
       }
       controller.delayMix = (val) => {
-        controller.delay.wet.value = parseFloat(val);
-        // console.log(val);
+        if (delayInvoked){
+          controller.delay.wet.value = parseFloat(val);
+          console.log(controller.delay.wet.value);
+        }
+        else delayInvoked = true;
       }
       controller.verbMix = (val) => {
-        controller.verb.wet.value = parseFloat(val);
-        // console.log(controller.verb.wet.value);
+        if (verbInvoked){
+          controller.verb.wet.value = parseFloat(val);
+          console.log(controller.verb.wet.value);
+        }
+        else verbInvoked = true;
       }
 
       controller.getUrl = () => {
